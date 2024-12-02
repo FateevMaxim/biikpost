6c3a871da4ce5c1292457f00177d63ef24d976ea
FateevMaxim <fateev.maxim@gmail.com>
2024-12-03 00:19:01 +0500

    new file upload schema

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -46,8 +46,7 @@
 
                         <div class="bg-[#b6da3a82] p-2 mt-4 mb-4">
                             <p class="mb-6 text-sm text-dark font-bold">Адрес склада в Китае</p>
-                            <p class="mb-6 text-sm text-dark" id="china">@yield( 'chinaaddress' ) <br />
-                                <b>({{ \Illuminate\Support\Facades\Auth::user()->login }}, {{ \Illuminate\Support\Facades\Auth::user()->name }}) </b><br />
+                            <p class="mb-6 text-sm text-dark" id="china">@yield( 'chinaaddress' )
                             </p>
                             <p class="mb-6 text-sm text-dark" style="display: none;" id="chinaaddress">@yield( 'chinaaddress' )({{ \Illuminate\Support\Facades\Auth::user()->login }}, {{ \Illuminate\Support\Facades\Auth::user()->name }})</p>
                             <button onclick="copyText()" class="focus:outline-none text-white bg-[#b6da3a] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Копировать</button>
